f4585ef80f574ba0eb06f162920031b9210d1f1f
Bo Thomsen <boh1996@gmail.com>
2013-10-14 14:58:08 +0200

    Comments and updated the sample config

--- config.sample.py
+++ config.sample.py
@@ -1,5 +1,38 @@
+# Database password
 db_password = ""
+# Database uername
 db_user     = ""
+# Database host, 127.0.0.1 etc
 db_host = ""
+# Schema name
 db_database = ""
+# Database system string, mysql+mysqldb etc
 database = "mysql+mysqldb"
+
+# Authentication redirect URL
+redirect_uri    = ""
+# Google Client ID
+client_id       = ""
+# Google Client Secret
+client_secret   = ""
+# Google Access token type
+access_type     = "offline"
+# Google OAuth flow
+approval_promt  = "force"
+# Google OAuth Scopes
+scopes          = [
+    "https://www.googleapis.com/auth/calendar",
+    "https://www.googleapis.com/auth/userinfo.profile",
+    "https://www.googleapis.com/auth/userinfo.email"
+]
+# Google Access token request type
+response_type   = "code"
+# Authentication base url
+base_url        = ""
+
+# Google Endpoint URLs
+google_auth_endpoint   = "https://accounts.google.com/o/oauth2/auth"
+google_token_endpoint   = "https://accounts.google.com/o/oauth2/token"
+google_revoke_endpont   = "https://accounts.google.com/o/oauth2/revoke"
+google_validate_url     = "https://www.googleapis.com/oauth2/v1/tokeninfo"
+google_user_info_url    = "https://www.googleapis.com/oauth2/v3/userinfo"
